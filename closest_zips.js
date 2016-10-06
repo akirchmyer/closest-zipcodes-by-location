b01475b5ed02dffb7f3fd91c6e0107ef918befac
Andrew Kirchmyer <akirchmyer@gmail.com>
2016-10-06 16:38:59 -0400

    initial commit

---
+++ closest_zips.js
@@ -0,0 +1,91 @@
+"use strict";
+
+let csv = require('fast-csv');
+
+let toRadians = (deg) => {
+	return deg * Math.PI / 180;
+}
+
+/** Extend Number object with method to convert numeric degrees to radians */
+if (Number.prototype.toRadians === undefined) {
+    Number.prototype.toRadians = function() { return this * Math.PI / 180; };
+}
+
+/**
+ * Creates a LatLon point on the earth's surface at the specified latitude / longitude.
+ *
+ * @constructor
+ * @param {number} lat - Latitude in degrees.
+ * @param {number} lon - Longitude in degrees.
+ *
+ * @example
+ *     var p1 = new LatLon(52.205, 0.119);
+ */
+function LatLon(lat, lon) {
+    // allow instantiation without 'new'
+    if (!(this instanceof LatLon)) return new LatLon(lat, lon);
+
+    this.lat = Number(lat);
+    this.lon = Number(lon);
+}
+
+
+/**
+ * Returns the distance from ‘this’ point to destination point (using haversine formula).
+ *
+ * @param   {LatLon} point - Latitude/longitude of destination point.
+ * @param   {number} [radius=6371e3] - (Mean) radius of earth (defaults to radius in metres).
+ * @returns {number} Distance between this point and destination point, in same units as radius.
+ *
+ * @example
+ *     var p1 = new LatLon(52.205, 0.119);
+ *     var p2 = new LatLon(48.857, 2.351);
+ *     var d = p1.distanceTo(p2); // 404.3 km
+ */
+LatLon.prototype.distanceTo = function(point, radius) {
+    if (!(point instanceof LatLon)) throw new TypeError('point is not LatLon object');
+    radius = (radius === undefined) ? 6371e3 : Number(radius);
+
+    var R = radius;
+    var φ1 = this.lat.toRadians(),  λ1 = this.lon.toRadians();
+    var φ2 = point.lat.toRadians(), λ2 = point.lon.toRadians();
+    var Δφ = φ2 - φ1;
+    var Δλ = λ2 - λ1;
+
+    var a = Math.sin(Δφ/2) * Math.sin(Δφ/2)
+          + Math.cos(φ1) * Math.cos(φ2)
+          * Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c;
+
+    return d;
+};
+
+
+let zip = process.argv[2];
+let count = process.argv[3];
+let locations = [];
+let selected;
+
+csv.fromPath('./data/zip_codes.csv', { headers: true })
+	.on('data', (data) => {
+		data.LatLon = LatLon(data.LAT, data.LNG);
+		if (zip === data.ZIP) {
+			selected = data;
+		}
+		locations.push(data)
+	})
+	.on('end', () => {
+		// Assign each location a distance value
+		locations.forEach((loc) => {
+			loc.dist = loc.LatLon.distanceTo(selected.LatLon);
+		});
+
+		// sort by distance ascending
+		locations.sort((a, b) => {
+			return a.dist - b.dist;
+		});
+
+		// Start at index 1 because 0 will be the selected zip code
+		process.stdout.write(JSON.stringify(locations.slice(1, count)));
+	});
